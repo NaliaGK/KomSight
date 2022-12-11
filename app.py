3daa9adb06e10479f4674188e0fcd260e84f7f6f
NaliaGK <74234522+NaliaGK@users.noreply.github.com>
2022-12-11 22:27:42 +0700

    3rd Commit

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, request, render_template
 import pickle
 import News_retriever
 import sentiment
-import WordCloud
 import Summary
 #objective
 app = Flask(__name__)
@@ -23,13 +22,12 @@ def predict():
     if request.method == 'POST':
         artikel, judul = News_retriever.getter(request.form['link kompas'])
         sentimen_result = sentiment.sen(artikel)
+        import WordCloud
         WordCloud.wordcloud(artikel, sentimen_result)
         summary = Summary.print_summary(artikel)
     else:
         artikel = 'pls wait a sec and click submit again'    
     return render_template('predict.html', artikel=artikel, judul=judul, sentimen_result=sentimen_result, summary=summary)
-
-
 
 
 if __name__ == "__main__":
